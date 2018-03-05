a838008e688dedcdf0dff509b2f1d2a7d0966a77
Markuss Baumgarts <markuss.baumgarts@gmail.com>
2018-03-05 16:06:40 +0000

    chahge mysql pass

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host : 'localhost',
 	user: 'root',
-	password : 'LAMPAmirgo42',
+	password : 'toor',
 	database : 'propel'
 });
 
