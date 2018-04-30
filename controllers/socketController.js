3b73955a1aeeb0dedd48573ccc2f81d67322252c
Kuber Kalra <kuber_kalra@ymail.com>
2018-04-30 16:43:51 +0100

    added user name

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,4 +1,4 @@
-module.exports = function(app,express,server, user_fb_id,Player,initPack,removePack){
+module.exports = function(app,express,server, user_fb_id,Player,initPack,removePack,user_fb_name){
 var io = require('socket.io')(server);
 
 io.set('heartbeat timeout',5000000);
@@ -13,7 +13,7 @@ if(user_fb_id){
 	socket_list[socket.id] = socket;
 	if(!(user_fb_id in Player.list)){
 		Player.onConnect(socket);
-		console.log("connection with id " + socket.id + " is connected" );
+		console.log("connection with id " + socket.id + ", " + user_fb_name " + is connected" );
 	}
 
   socket.on('disconnect', (reason)=>{ //Delete the player form the socket and player list when it desconnect. It's an automatic function, no need to emit
@@ -25,7 +25,7 @@ if(user_fb_id){
     console.log(Player.list);
     user_fb_id = null;
 	
-    console.log('Connection ' + socket.id + " is disconnected " + reason);
+    console.log('Connection ' + socket.id + ", " + user_fb_name  + " is disconnected " + reason);
      }
   });
  // console.log('Socket Connection with unique id ' + socket.id + " is connected");
@@ -43,7 +43,7 @@ app.get('/logout', function (req, res) {
 	console.log(Player.list);
     user_fb_id = null;
 	
-    console.log('Connection ' + sockety.id + " is disconnected ");
+    console.log('Connection ' + sockety.id + ", " + user_fb_name + " is disconnected ");
         // destroy session data
         req.session = null;
 	
