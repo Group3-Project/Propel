b31ab95dfd81c0b0317c71eab8abf11c7b488d97
Kuber Kalra <kuber_kalra@ymail.com>
2018-05-01 13:20:20 +0100

    clean

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -29,34 +29,18 @@ io.sockets.on('connection', (socket)=>{ //Whenever a player connect
       console.log("-------------------------------");
         
     })
-    /*socket.on('disconnect', (reason)=>{ //Delete the player form the socket and player list when it desconnect. It's an automatic function, no need to emit
-       if(user_fb_id){
-         delete socket_list[socket.id];
-          Player.onDisconnect(socket);
-          console.log("On diss");
-          user_fb_id = null;
-          console.log(Player.list);
-          console.log('Connection ' + socket.id + ", " + user_fb_name  + " is disconnected " + reason);
-      }
-      //Possible solution to random disconnects:
-      // * try to reconect like 5 times or for 10 sec
-      // if you fail -> disconnect and alles gut
-    });*/
-   }
+   //}
 });
 
-
 setInterval(()=>{
-  var pack = Player.update();
-
-  for(var i in socket_list){
-    var socket = socket_list[i];
-    socket.emit('init',initPack);
+	var pack = Player.update();
+	for(var i in socket_list){
+    		var socket = socket_list[i];
+    		socket.emit('init',initPack);
 		socket.emit('update',pack);
 		socket.emit('remove',removePack);
-  }
-  initPack.player = [];
-  removePack.player = [];
-},1000);
-
+  	}
+  	initPack.player = [];
+  	removePack.player = [];
+    },1000);
 }
