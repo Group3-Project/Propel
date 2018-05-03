b3fb9226add7b2ae8a8b4a0ef46f86c79da4e3f3
OllieSY <30958551+OllieSY@users.noreply.github.com>
2018-05-03 15:39:15 +0100

    Update chatController.js

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -20,9 +20,7 @@ io.sockets.on('connection', function(socket){
               	 'name' : Object.keys(users)[i],
               	  'id' : users[Object.keys(users)[i]].id
             }
-		if(temp_list.indexOf(tempObj) > -1){
-		    temp_list.push(tempObj)
-		}		
+		console.log(temp_list.indexOf(tempObj));	
        	 }
 	    
 	
