5c6bfd425e8a56e9030580e764ee1cb239fa0729
manky880 <33086889+manky880@users.noreply.github.com>
2018-05-03 13:41:17 +0100

    Update chatController.js

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -6,8 +6,10 @@ var users = {};
 io.sockets.on('connection', function(socket){
 
    
-     if(!(user_fb_name in users)){
-	    socket.nickname = user_fb_name;
+     if(user_fb_name in users){
+	    break;
+     }else{
+	     socket.nickname = user_fb_name;
             users[socket.nickname] = socket;
             updateNicknames();
      }
