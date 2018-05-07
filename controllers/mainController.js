8b0adfca051c602151d651cc5500d7a33049d61f
OllieSY <30958551+OllieSY@users.noreply.github.com>
2018-05-07 14:20:55 +0100

    Update mainController.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -135,7 +135,7 @@ app.get('/profile/:username',function(req, res){
 	user_profile = req.user;
 	var friends = false;
 	console.log(req.params.username)
-	var promise_user = getData("select * from users where fb_id =" + req.params.username);
+	var promise_user = getData("select * from users where fb_id =" + user_profile.id);
 	promise_user.then(function(dataUser){
 			var promise_friends = getData("select * from user_friends s1 LEFT JOIN users s2 ON s1.friend_id = s2.fb_id WHERE s1.user_id =" + req.params.username);
 			promise_friends.then(function(dataFriends){
