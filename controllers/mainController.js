531ab4833742dfda8597c766e09385337d1a76e3
OllieSY <30958551+OllieSY@users.noreply.github.com>
2018-05-07 15:41:51 +0100

    Update mainController.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -138,6 +138,7 @@ app.get('/profile/:username',function(req, res){
 				if(req.user){
 					var promiseisfriend = getData("select * from user_friends where friend_id = " + req.params.username + " and user_id = " + user_profile.id);
 						promiseisfriend.then(function(dataIsFriend){
+							console.log(typeof dataIsFriend != 'undefined');
 							if(typeof dataIsFriend != 'undefined'){
 								friends = true;
 							}
