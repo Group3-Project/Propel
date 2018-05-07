e4d3ca0fe28d5b3b400e3db654e1b3fa3a31bc6c
OllieSY <30958551+OllieSY@users.noreply.github.com>
2018-05-07 14:04:50 +0100

    Update mainController.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -107,7 +107,7 @@ app.get('/profile',function(req, res){
 		 user_profile = req.user;
 		 socketController(app, express,server,user_profile.id,Player,initPack,removePack,user_profile.name,io,DB);
 		 user_profile.fb_id = user_profile.id;
-		 var promise_friends = getData("select * from user_friends s1 LEFT JOIN users s2 ON s1.friend_id = s2.fb_id WHERE s1.user_id =" + req.params.username);
+		 var promise_friends = getData("select * from user_friends s1 LEFT JOIN users s2 ON s1.friend_id = s2.fb_id WHERE s1.user_id =" + user_profile.id);
 			promise_friends.then(function(dataFriends){
 					for (i = 0; i < dataFriends.length; i++){			
 						if (dataFriends[i].friend_id == user_profile.id){
