72f927f8f8d7edaef0736a87146c1dd80dfea2db
manky880 <33086889+manky880@users.noreply.github.com>
2018-05-08 15:19:09 +0100

    Update chatController.js

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -33,7 +33,7 @@ io.sockets.on('connection', function(socket){
         var msg = data.trim();
         if(msg.substr(0,3) === '/w '){
             msg = msg.substr(3);
-            var ind = msg.indexOf('/');
+            var ind = msg.indexOf(' /');
             if(ind !== -1){
                 var name = msg.substring(0, ind);
                 var msg = msg.substring(ind + 2);
