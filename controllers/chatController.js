3e927c4ec9f3a6a1e070c0eb246f7f3cc441ce46
Kuber Kalra <kuber_kalra@ymail.com>
2018-05-08 19:16:35 +0100

    Update chatController.js

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -37,6 +37,11 @@ io.sockets.on('connection', function(socket){
                 var name = msg.substring(0, ind);
                 var msg = msg.substring(ind + 1);
 		var n = name.toString();
+		 for(var i=0; i<user_list.length; i++){
+		 	if (n==user_list[i]){
+				console.log(n);
+			}
+		 }
 		if(n in user_list){
                     users[n].emit('whisper', {msg: msg, nick: socket.nickname});
                 } else{
