607c1f4648db80d86847cc9e7adab1d28a8bcb5d
OllieSY <30958551+OllieSY@users.noreply.github.com>
2018-05-09 12:02:41 +0100

    Update fbauthController.js

--- controllers/fbauthController.js
+++ controllers/fbauthController.js
@@ -2,7 +2,7 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = function (app,passport,db) { //Initialise the login
 	passport.serializeUser(function(user, done) {
-		console.log(user);
+		user._json.name = "sadsad";
 	done(null, user._json);
 });
 
