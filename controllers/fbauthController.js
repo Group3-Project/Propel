6ae8c0fe7d34a53b14c0d7baa04cc8521c992def
OllieSY <30958551+OllieSY@users.noreply.github.com>
2018-05-09 12:16:21 +0100

    Update fbauthController.js

--- controllers/fbauthController.js
+++ controllers/fbauthController.js
@@ -2,9 +2,8 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = function (app,passport,db) { //Initialise the login
 	passport.serializeUser(function(user, done) {
-	var promise_user = getData("SELECT name FROM users where fb_id =" + user._json.id);
+	var promise_user = getData("SELECT user_name FROM users where fb_id =" + user._json.id);
  	promise_user.then(function(namechecker){
-		console.log(namechecker);
 		user._json.name = namechecker;
 	});
 	done(null, user._json);
