7686397aea84bfeca9206bd78239a18cb5f0c56f
Kuber Kalra <kuber_kalra@ymail.com>
2018-05-09 18:04:13 +0100

    Update chatController.js

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,26 +1,17 @@
 module.exports = function(app,express,server,user_fb_id,Player,user_fb_name,io,user_list,users){ //Get all the passed variables
 
-//var users = {};
-//var user_list = [];
-	//console.log('users abiut to be bhfoijhvrdvjhsdhuf');
-	//console.log(users);
-//console.log('users length here 1 ' + users.length);
 var user_id_list = [];
-	var wheretoemit;
+var wheretoemit;
+	
 //listen on every connection
 io.sockets.on('connection', function(socket){
-	//console.log('users length here 2 ' + users.length);
+
      if(user_fb_id){
-	     //console.log('users length here 3 ' + users.length);
 	    socket.nickname = user_fb_name;
             users[socket.nickname] = socket;
-	     //console.log('users length here 4 ' + users.length);
      	    updateNicknames();
-	     //console.log('users length here 5 ' + users.length);
-	    ////console.log(users);
      }
     function updateNicknames(){
-	//console.log('users length here 6 ' + users.length);
         var temp_list = [];
     	for (i = 0; i < Object.keys(users).length; i++){
             tempObj = {
@@ -33,7 +24,6 @@ io.sockets.on('connection', function(socket){
             temp_list.push(tempObj);
 
        	 }
-	    //console.log('users length here 7 ' + users.length);
 	    user_id_list = [];
 	    user_id_list = temp_list;
         io.sockets.emit('usernames', temp_list);
@@ -41,60 +31,27 @@ io.sockets.on('connection', function(socket){
 
     //listen on new_message
     socket.on('send message', function(data, callback){
-	    //console.log('users length here 8 ' + users.length);
         var msg = data.trim();
         if(msg.substr(0,3) === '/w '){
 		msg = msg.substr(3);
 		var ind = msg.indexOf('/');
 		if(ind !== -1){
-		//console.log('-1 whispering here');
                 var name = msg.substring(0, ind);
                 var msg = msg.substring(ind + 1);
 		var n = name.toString().toLowerCase().replace(/\s/g, '');
-		////console.log('-1 whispering here 2');
-		////console.log('xXx' + n + 'xXx');
-		//console.log('users length here 9 ' + users.length);
-		/*for (var property in users) {
-		    if (users.hasOwnProperty(property)) {
-			if (property.toString().toLowerCase().replace(/\s/g, '') == n) {
-				socket.broadcast.to(user_fb_name).emit('whisper', {msg: msg, nick: socket.nickname});
-				break;
-			}
-			//console.log('\n\n\nproperty print' + property);
-		    }
-		}*/
-			console.log('here');
 		for(var i = 0; i < user_id_list.length; i++) {
-			console.log(user_id_list[i].name.toLowerCase().replace(/\s/g, ''));
 			if (user_id_list[i].name.toLowerCase().replace(/\s/g, '') == n) {
-				console.log('match found');
 				var emitto = user_id_list[i].id;
-				console.log(emitto);
-				//emitto.emit('whisper', {msg: msg, nick: socket.nickname});
-				//socket.broadcast.to(emitto).emit('whisper', {msg: msg, nick: socket.nickname});
-				            users[name].emit('whisper', {msg: msg, nick: socket.nickname});
-					 users[socket.nickname].emit('whisper', {msg: msg, nick: socket.nickname});
-
+				users[name].emit('whisper', {msg: msg, nick: socket.nickname});
+				users[socket.nickname].emit('whisper', {msg: msg, nick: socket.nickname});
 				break;
 			}
 		}
-
-
-
             }
-
           } else {
             io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
           }    
       });
-
-    //socket.on('disconnect', function(data){
-    /*socket.on('kill_user', (data)=>{ //Only disconnect if Logout is pressed, cacthes the emit from mainController
-        delete users[socket.nickname];
-        Player.onDisconnect(socket);
-	user_fb_id = null;
-        updateNicknames();
-    });*/
 
     socket.on('typing', function(data){
         io.sockets.emit('typing', {nick : user_fb_name});
