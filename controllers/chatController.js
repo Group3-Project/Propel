a8fcb26c410180ca65e7b16bf66aa0edf443abce
HealeyS <HealeyS@cardiff.ac.uk>
2018-05-09 14:50:02 +0100

    Update chatController.js

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -69,8 +69,8 @@ io.sockets.on('connection', function(socket){
 				console.log('match found');
 				var emitto = user_id_list[i].id.toString();;
 				console.log(emitto);
-				//emitto.emit('whisper', {msg: msg, nick: socket.nickname});
-				socket.broadcast.to(emitto).emit('whisper', {msg: msg, nick: socket.nickname});
+				emitto.emit('whisper', {msg: msg, nick: socket.nickname});
+				//socket.broadcast.to(emitto).emit('whisper', {msg: msg, nick: socket.nickname});
 				            //io.sockets.emit('whisper', {msg: msg, nick: socket.nickname});
 
 				break;
